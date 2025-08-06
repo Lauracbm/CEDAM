b8d8b8f310164f4322200050550ffe740787848a
LauraCedam <bolivarmelolaura@gmail.com>
2025-08-06 11:09:20 -0500

    logo cedam hero

--- Js/parallax.js
+++ Js/parallax.js
@@ -1,13 +1,15 @@
 window.onscroll = () => {
     /*parallax hero*/
-    document.querySelector(".cloud3").style.marginTop = -scrollY +"px";
+    document.querySelector(".cloud3").style.marginTop = scrollY +"px";
     document.querySelector(".cloudL").style.marginRight = scrollY +.5 + "px";
     document.querySelector(".cloudR").style.marginLeft = scrollY +.9 + "px";
     document.querySelector(".cloud2").style.marginTop = -scrollY +"px";
     document.querySelector(".cloud1").style.marginTop = -scrollY +"px";
     document.querySelector(".main").style.marginTop = -scrollY + .8 + "px";
-    /*document.querySelector(".hero h1").style.marginTop = scrollY + "px";
-    document.querySelector(".hero h1").style.opacity = ( 100 - scrollY) / 100;*/
+    document.querySelector(".hero h1").style.marginBottom = scrollY + "px";
+    document.querySelector(".hero h1").style.opacity = ( 100 - scrollY) / 50;
+    document.querySelector(".cLogo").style.opacity = ( 100 - scrollY) / 50;
+    document.querySelector(".camilacrayon").style.marginRight = scrollY + "px";
 
     /*Parallax */
     document.querySelector(".pp").style.opacity = scrollY / 100;
