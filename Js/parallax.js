f32f28f12a55a503cde2793ede2e2b31d563f360
LauraCedam <bolivarmelolaura@gmail.com>
2025-11-06 15:18:53 -0500

    hero texto xolor arreglado

--- Js/parallax.js
+++ Js/parallax.js
@@ -1,15 +1,13 @@
 window.onscroll = () => {
     /*parallax hero*/
-    
-   document.querySelector(".camilo_flying").style.marginTop = -scrollY +"px";
-    document.querySelector(".cloud1").style.marginTop = -scrollY +"px";
-    document.querySelector(".planet").style.marginTop = scrollY +"px";
+
+    document.querySelector(".camilo_flying").style.marginTop = -scrollY + "px";
+    document.querySelector(".camila_flying").style.marginTop = -scrollY + "px";
+    document.querySelector(".cloud_group").style.marginTop = -scrollY + "px";
+    document.querySelector(".planet").style.marginTop = scrollY + "px";
+    document.querySelector(".Cedam_name").style.opacity = (100 - scrollY) / 50;
+    document.querySelector(".cLogo").style.opacity = (100 - scrollY) / 50;
     document.querySelector(".hero h1").style.marginBottom = scrollY + "px";
-    document.querySelector(".hero h1").style.opacity = ( 100 - scrollY) / 50;
-    document.querySelector(".cLogo").style.opacity = ( 100 - scrollY) / 50;
-
-    /*Parallax */
-    document.querySelector(".pp").style.opacity = scrollY / 100;
 
 
-} 
+}
